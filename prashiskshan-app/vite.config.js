24e336925fc280ec62aa43b8790bca8129497472
sumitkumar112006 <amitk839170@gmail.com>
2025-09-18 22:27:19 +0530

    delete

--- prashiskshan-app/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: '/SIH_2025/', // GitHub Pages deployment
-})
